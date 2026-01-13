16482a5aeb758160ce77b4aa22c6ae5db3ca30cb
Bilal Yousaf <aptech0320@gmail.com>
2026-01-13 05:25:40 +0500

    fixed horizontal scrollbar

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,8 @@
 const Albums_Btn = document.getElementById("btn-aln");
 const Extra_Album = document.getElementsByClassName("Extra_Album");
 
-Albums_Btn.addEventListener("click", function () {
-    for(let album of Extra_Album){
-        album.classList.toggle("show");
+Albums_Btn.addEventListener("click", () => {
+    for(let albums of Extra_Album){
+        albums.classList.toggle("show");
     }
 })
